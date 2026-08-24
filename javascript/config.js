ddc32b8324ada5dea68bb2eaa70d0eb1fdc2d963
Letícia Marcolino <leticiam650@gmail.com>
2026-08-24 19:29:58 -0300

    Update config.js

--- javascript/config.js
+++ javascript/config.js
@@ -8,7 +8,7 @@ const SUPABASE_URL = "https://dvocbwecshpvcilejfvt.supabase.co/rest/v1/";
 
 // Cole aqui a chave "anon public"
 // (Project Settings > API > Project API keys > anon public)
-const SUPABASE_ANON_KEY = "sb_secret_cXxh_xWUfhgbHIdfTykeUg_Bx9FXdy5";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2b2Nid2Vjc2hwdmNpbGVqZnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDM3MTEsImV4cCI6MjEwMzE3OTcxMX0.kjpQorhGHsOmdg6T8CTl9cudNZ8e-LQj28DcjxrjRYM";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
